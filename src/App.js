84523280a80d62a4712aae1ac984be6e0954779c
kindrew <kindrew99@gmail.com>
2023-10-09 22:20:01 +0000

    update

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ import Home from "./container/home";
 import Party from "./container/letsparty";
 import OurStory from "./container/ourstory";
 import { Link as ScrollLink, Element } from "react-scroll";
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navabr />
+    <Router>
+      <div className="App">
+        <Navabr />
 
-      <Element name="home">
-        <Home />
-      </Element>
+        <Element name="home">
+          <Home />
+        </Element>
 
-      <Brochure />
-    </div>
+        <Brochure />
+      </div>
+    </Router>
   );
 }
 
