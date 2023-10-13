86dfcc9b569c62a45bba5b0a2f3516d326754d2d
kindrew <kindrew99@gmail.com>
2023-10-13 11:51:04 +0000

    update

---
+++ src/components/background.js
@@ -0,0 +1,38 @@
+import "./background.css";
+import underline from "../asserts/Screenshot 2023-10-08 085433.png";
+import back from "../asserts/030.jpg";
+import front from "../asserts/0101.png";
+import middle from "../asserts/0202.png";
+
+const Background = () => {
+  return (
+    <div className="bg">
+      <img className="wandecoal" src={back} alt="" />
+      <img className="burnaboy" src={middle} alt="" />
+      <img className="wizkid" src={front} alt="" />
+      <div className="fireboy">
+        <span class="heading">Details</span>
+        <img class="underline" src={underline} alt="" />
+        <span class="subheading">ceremony</span>
+        <p class="content1">
+          Trinity Methodist Church <br />
+          Kwadaso Estate,Kumasi
+        </p>
+
+        <span class="subheading">reception</span>
+        <p class="content1">
+          424 A, Kwadaso Estate <br />
+          Digital Address: AK-226-2249
+        </p>
+
+        <span class="subheading rsvp">RSVP</span>
+        <p class="content1">
+          Maame Serwaa - 0504712906 <br />
+          Ella Asabere - 0541476590
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Background;
--- src/container/brochure.js
+++ src/container/brochure.js
@@ -2,12 +2,14 @@ import FirstPage from "../components/firstpage";
 import SecondPage from "../components/secondpage";
 import "../components/navbar";
 import Third from "../components/third";
+import Background from "../components/background";
 
 const Brochure = () => {
   return (
     <div className="brochure">
       <FirstPage />
       <Third />
+      <Background />
     </div>
   );
 };
