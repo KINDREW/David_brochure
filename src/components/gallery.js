03e045edaef9a4b84e715affe782cb70423106dc
kindrew <kindrew99@gmail.com>
2023-10-20 19:17:23 +0000

    update

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,17 +1,29 @@
 import "./gallery.css";
-import going from "../asserts/IMG-20181002-WA0019-COLLAGE.jpg";
-import gone from "../asserts/IMG-20181002-WA0019-PHOTO_FRAME.jpg";
+import one from "../asserts/20220102_192529-PHOTO_FRAME.jpg";
+import two from "../asserts/20221007_170527-PHOTO_FRAME.jpg";
+import three from "../asserts/IMG-20181002-WA0019-PHOTO_FRAME.jpg";
+import four from "../asserts/IMG-20181021-WA0006-PHOTO_FRAME.jpg";
+import five from "../asserts/IMG-20190512-WA0079-PHOTO_FRAME.jpg";
+import six from "../asserts/IMG-20200308-WA0078-PHOTO_FRAME.jpg";
+import seven from "../asserts/IMG-20220406-WA0011-PHOTO_FRAME.jpg";
 
 const Gallery = () => {
   return (
     <div className="gallery">
       <div className="started">
-        <h1>How it started</h1>
-        <img src={going} alt="" />
-      </div>
-      <div className="going">
-        <h1>How it is going...</h1>
-        <img src={gone} alt="" />
+        <h1>How it started...</h1>
+        <div className="lefttt">
+          <img src={one} alt="" />
+          <img src={four} alt="" />
+        </div>
+        <div className="middleeee">
+          <img src={two} alt="" />
+          <img src={five} alt="" />
+        </div>
+        <div className="rightttttt">
+          <img src={three} alt="" />
+          <img src={six} alt="" />
+        </div>
       </div>
     </div>
   );
