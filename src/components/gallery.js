7dad3af08bba8b1ffce2b07980a6601b5b1ad9dd
kindrew <kindrew99@gmail.com>
2023-10-20 18:20:08 +0000

    update

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,10 +1,18 @@
+import "./gallery.css";
+import going from "../asserts/IMG-20181002-WA0019-COLLAGE.jpg";
+import gone from "../asserts/IMG-20181002-WA0019-PHOTO_FRAME.jpg";
+
 const Gallery = () => {
   return (
     <div className="gallery">
-      <h1>Photos of the event would be posted here</h1>
-      <p>
-        Relax na wo pri do do <br /> <h3>fior fior</h3>
-      </p>
+      <div className="started">
+        <h1>How it started</h1>
+        <img src={going} alt="" />
+      </div>
+      <div className="going">
+        <h1>How it is going...</h1>
+        <img src={gone} alt="" />
+      </div>
     </div>
   );
 };
--- src/container/home.js
+++ src/container/home.js
@@ -11,6 +11,7 @@ const Home = () => {
         <span className="names">David & Nana Yaa</span>
         <span className="getting">Are Getting Married</span>
       </div>
+
       <Element name="ourstory">
         <OurStory />
       </Element>
